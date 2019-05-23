6a315070b0104132691613f5bb4d4f9580f1cdb3
Maruhay <denismarush@gmail.com>
2019-05-23 10:34:44 +0200

    Tests configuration

--- core/src/com/pio/battleship/Tests.java
+++ core/src/com/pio/battleship/Tests.java
@@ -1,15 +1,17 @@
 package com.pio.battleship;
 
 import com.badlogic.gdx.graphics.Color;
+import jdk.internal.jline.internal.TestAccessible;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class Tests {
 
     @Test
-    public void aircraftCarrierPick ( ) {
+    public void aircraftCarrierPick() {
 
-        Coordinates [] myCoordinatesA = new Coordinates[4];
+        Coordinates[] myCoordinatesA = new Coordinates[4];
         myCoordinatesA[0] = new Coordinates();
         myCoordinatesA[1] = new Coordinates();
         myCoordinatesA[2] = new Coordinates();
@@ -24,49 +26,49 @@ public class Tests {
         myCoordinatesA[3].setX(7);
         myCoordinatesA[3].setY(8);
 
-        AircraftCarrier myAircraftCarrier = new AircraftCarrier(myCoordinatesA,Color.RED);
+        AircraftCarrier myAircraftCarrier = new AircraftCarrier(myCoordinatesA, Color.RED);
         Coordinates myCoordinatesB = new Coordinates();
 
         myCoordinatesB.setX(7);
         myCoordinatesB.setY(3);
-        assertEquals(myAircraftCarrier.pick(myCoordinatesB,false),1);
+        assertEquals(myAircraftCarrier.pick(myCoordinatesB, false), 1);
 
         myCoordinatesB.setX(6);
         myCoordinatesB.setY(8);
-        assertEquals(myAircraftCarrier.pick(myCoordinatesB,false),2);
+        assertEquals(myAircraftCarrier.pick(myCoordinatesB, false), 2);
 
         myCoordinatesB.setX(4);
         myCoordinatesB.setY(8);
-        assertEquals(myAircraftCarrier.pick(myCoordinatesB,false),2);
+        assertEquals(myAircraftCarrier.pick(myCoordinatesB, false), 2);
 
         myCoordinatesB.setX(7);
         myCoordinatesB.setY(8);
-        assertEquals(myAircraftCarrier.pick(myCoordinatesB,false),2);
+        assertEquals(myAircraftCarrier.pick(myCoordinatesB, false), 2);
 
         myCoordinatesB.setX(5);
         myCoordinatesB.setY(8);
-        assertEquals(myAircraftCarrier.pick(myCoordinatesB,true),2);
+        assertEquals(myAircraftCarrier.pick(myCoordinatesB, true), 2);
 
         myCoordinatesB.setX(5);
         myCoordinatesB.setY(8);
-        assertEquals(myAircraftCarrier.pick(myCoordinatesB,false),3);
+        assertEquals(myAircraftCarrier.pick(myCoordinatesB, false), 3);
 
-        }
+    }
 
     @Test
-    public void boardAreAllShipsSunk ( ) {
+    public void boardAreAllShipsSunk() {
 
-        Board myBoard = new Board(10,10);
+        Board myBoard = new Board(10, 10);
 
         Coordinates myCoordinatesA = new Coordinates();
         myCoordinatesA.setX(6);
         myCoordinatesA.setY(4);
-        myBoard.addShip(new SmallBoat(myCoordinatesA,Color.RED));
+        myBoard.addShip(new SmallBoat(myCoordinatesA, Color.RED));
 
         Coordinates myCoordinatesB = new Coordinates();
         myCoordinatesB.setX(3);
         myCoordinatesB.setY(7);
-        myBoard.addShip(new SmallBoat(myCoordinatesB,Color.RED));
+        myBoard.addShip(new SmallBoat(myCoordinatesB, Color.RED));
         assertFalse(myBoard.areAllShipsSunk());
 
         Coordinates myCoordinatesC = new Coordinates();
@@ -80,37 +82,37 @@ public class Tests {
         myBoard.pick(myCoordinatesC);
         assertTrue(myBoard.areAllShipsSunk());
 
-        }
+    }
 
     @Test
-    public void boardClear ( ) {
+    public void boardClear() {
 
-        Board myBoard = new Board(10,10);
+        Board myBoard = new Board(10, 10);
 
-        assertEquals(myBoard.grid[0][0],0);
-        assertEquals(myBoard.grid[5][5],0);
-        assertEquals(myBoard.grid[9][9],0);
+        assertEquals(myBoard.grid[0][0], 0);
+        assertEquals(myBoard.grid[5][5], 0);
+        assertEquals(myBoard.grid[9][9], 0);
 
         myBoard.grid[0][0] = 1;
         myBoard.grid[5][5] = 2;
         myBoard.grid[9][9] = 3;
 
-        assertEquals(myBoard.grid[0][0],1);
-        assertEquals(myBoard.grid[5][5],2);
-        assertEquals(myBoard.grid[9][9],3);
+        assertEquals(myBoard.grid[0][0], 1);
+        assertEquals(myBoard.grid[5][5], 2);
+        assertEquals(myBoard.grid[9][9], 3);
 
         myBoard.clear();
 
-        assertEquals(myBoard.grid[0][0],0);
-        assertEquals(myBoard.grid[5][5],0);
-        assertEquals(myBoard.grid[9][9],0);
+        assertEquals(myBoard.grid[0][0], 0);
+        assertEquals(myBoard.grid[5][5], 0);
+        assertEquals(myBoard.grid[9][9], 0);
 
-        }
+    }
 
     @Test
-    public void corvetteIsSunk ( ) {
+    public void corvetteIsSunk() {
 
-        Coordinates [] myCoordinatesA = new Coordinates[2];
+        Coordinates[] myCoordinatesA = new Coordinates[2];
         myCoordinatesA[0] = new Coordinates();
         myCoordinatesA[1] = new Coordinates();
         myCoordinatesA[0].setX(2);
@@ -118,29 +120,29 @@ public class Tests {
         myCoordinatesA[1].setX(9);
         myCoordinatesA[1].setY(4);
 
-        Corvette myCorvette = new Corvette(myCoordinatesA,Color.RED);
+        Corvette myCorvette = new Corvette(myCoordinatesA, Color.RED);
         assertFalse(myCorvette.isSunk());
 
         Coordinates myCoordinatesB = new Coordinates();
         myCoordinatesB.setX(5);
         myCoordinatesB.setY(3);
-        myCorvette.pick(myCoordinatesB,false);
+        myCorvette.pick(myCoordinatesB, false);
         assertFalse(myCorvette.isSunk());
 
         myCoordinatesB.setX(9);
         myCoordinatesB.setY(4);
-        myCorvette.pick(myCoordinatesB,false);
+        myCorvette.pick(myCoordinatesB, false);
         assertFalse(myCorvette.isSunk());
 
         myCoordinatesB.setX(2);
         myCoordinatesB.setY(7);
-        myCorvette.pick(myCoordinatesB,false);
+        myCorvette.pick(myCoordinatesB, false);
         assertTrue(myCorvette.isSunk());
 
-        }
+    }
 
     @Test
-    public void destroyerFactoryGetShip ( ) {
+    public void destroyerFactoryGetShip() {
 
         DestroyerFactory myDestroyerFactory = new DestroyerFactory();
         Coordinates myCoordinatesA = new Coordinates();
@@ -151,28 +153,28 @@ public class Tests {
         myCoordinatesB.setX(5);
         myCoordinatesB.setY(7);
 
-        Ship myShip = myDestroyerFactory.getShip(myCoordinatesA,myCoordinatesB,Color.RED);
+        Ship myShip = myDestroyerFactory.getShip(myCoordinatesA, myCoordinatesB, Color.RED);
         assertNotNull(myShip);
 
         Coordinates myCoordinatesC = new Coordinates();
         myCoordinatesC.setY(7);
 
         myCoordinatesC.setX(3);
-        myShip.pick(myCoordinatesC,false);
+        myShip.pick(myCoordinatesC, false);
         assertFalse(myShip.isSunk());
 
         myCoordinatesC.setX(4);
-        myShip.pick(myCoordinatesC,false);
+        myShip.pick(myCoordinatesC, false);
         assertFalse(myShip.isSunk());
 
         myCoordinatesC.setX(5);
-        myShip.pick(myCoordinatesC,false);
+        myShip.pick(myCoordinatesC, false);
         assertTrue(myShip.isSunk());
 
-        }
+    }
 
     @Test
-    public void eventListenerIsEventAvailable ( ) {
+    public void eventListenerIsEventAvailable() {
 
         EventListener myEventListener = new EventListener();
         assertFalse(myEventListener.isEventAvailable());
@@ -187,55 +189,55 @@ public class Tests {
         myEventListener.getEvent();
         assertFalse(myEventListener.isEventAvailable());
 
-        }
+    }
 
     @Test
-    public void eventListenerGetEvent ( ) {
+    public void eventListenerGetEvent() {
 
         EventListener myEventListener = new EventListener();
 
         myEventListener.keyDown(15);
-        myEventListener.touchDown(20,25,0,3);
+        myEventListener.touchDown(20, 25, 0, 3);
 
         Event myEventA = myEventListener.getEvent();
         Event myEventB = myEventListener.getEvent();
 
-        assertSame(myEventA.Type,Event.EventType.KeyPressed);
-        assertEquals(myEventA.Code,15);
-        assertSame(myEventB.Type,Event.EventType.MouseKeyPressed);
-        assertEquals(myEventB.Code,3);
-        assertEquals(myEventB.PositionX,20);
-        assertEquals(myEventB.PositionY,25);
+        assertSame(myEventA.Type, Event.EventType.KeyPressed);
+        assertEquals(myEventA.Code, 15);
+        assertSame(myEventB.Type, Event.EventType.MouseKeyPressed);
+        assertEquals(myEventB.Code, 3);
+        assertEquals(myEventB.PositionX, 20);
+        assertEquals(myEventB.PositionY, 25);
 
-        }
+    }
 
     @Test
-    public void gameGetGridCoordinates ( ) {
+    public void gameGetGridCoordinates() {
 
         Game myGame = new Game();
-    // comment
-        Coordinates myCoordinatesA = myGame.getGridCoordinates(50,150,100,100,600,600);
-        Coordinates myCoordinatesB = myGame.getGridCoordinates(150,50,100,100,600,600);
-        Coordinates myCoordinatesC = myGame.getGridCoordinates(750,150,100,100,600,600);
-        Coordinates myCoordinatesD = myGame.getGridCoordinates(150,750,100,100,600,600);
+        // comment
+        Coordinates myCoordinatesA = myGame.getGridCoordinates(50, 150, 100, 100, 600, 600);
+        Coordinates myCoordinatesB = myGame.getGridCoordinates(150, 50, 100, 100, 600, 600);
+        Coordinates myCoordinatesC = myGame.getGridCoordinates(750, 150, 100, 100, 600, 600);
+        Coordinates myCoordinatesD = myGame.getGridCoordinates(150, 750, 100, 100, 600, 600);
 
         assertNull(myCoordinatesA);
         assertNull(myCoordinatesB);
         assertNull(myCoordinatesC);
         assertNull(myCoordinatesD);
 
-        Coordinates myCoordinatesE = myGame.getGridCoordinates(437,283,100,100,600,600);
-        Coordinates myCoordinatesF = myGame.getGridCoordinates(172,684,100,100,600,600);
+        Coordinates myCoordinatesE = myGame.getGridCoordinates(437, 283, 100, 100, 600, 600);
+        Coordinates myCoordinatesF = myGame.getGridCoordinates(172, 684, 100, 100, 600, 600);
 
-        assertEquals(myCoordinatesE.getX(),5);
-        assertEquals(myCoordinatesE.getY(),3);
-        assertEquals(myCoordinatesF.getX(),1);
-        assertEquals(myCoordinatesF.getY(),9);
+        assertEquals(myCoordinatesE.getX(), 5);
+        assertEquals(myCoordinatesE.getY(), 3);
+        assertEquals(myCoordinatesF.getX(), 1);
+        assertEquals(myCoordinatesF.getY(), 9);
 
-        }
+    }
 
     @Test
-    public void playerGetColor ( ) {
+    public void playerGetColor() {
 
         Player myRedPlayer = new Player(Color.RED);
         Player myGreenPlayer = new Player(Color.GREEN);
@@ -245,14 +247,13 @@ public class Tests {
         assertSame(myGreenPlayer.getColor(), Color.GREEN);
         assertSame(myBluePlayer.getColor(), Color.BLUE);
 
-        }
+    }
 
     @Test
-    public void playerGetBoard ( ) {
+    public void playerGetBoard() {
         //comment
         Player myPlayer = new Player(Color.RED);
         assertNotNull(myPlayer.getBoard());
 
-        }
-
     }
+}
